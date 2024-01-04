a53eb3ea9c7139bdddcc75a985e248f7553a97ef
Sahishnuta <50331193+Sahishnuta@users.noreply.github.com>
2024-01-04 23:39:38 +0100

    Add files via upload

---
+++ End_to_end_Medical_Chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sahishnuta Tosh',
+    author_email= 'sahishnutaatosh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
